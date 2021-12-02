fef994c5a14ea484e200628fad4f83c3341ee1a9
Jabbawocker <50668928+Jabbawocker@users.noreply.github.com>
2021-12-02 14:23:06 -0500

    Add files via upload

--- bulk-service.js
+++ bulk-service.js
@@ -5,20 +5,21 @@ PagerDuty.api({token: 'su+5n_XdrViGnYFELTyxQ', endpoint: '/services'})
 
   var api_version = "2";
   $.ajax({
-    url: "https://api.pagerduty.com",
-    type: "GET",
+    url: "https://api.pagerduty.com/services",
+    type: "POST",
     headers: {"Accept": "application/vnd.pagerduty+json;version=" + api_version}
-  });
-
-var fileInput = document.getElementById("csv-file-input")
+  }); 
 
 fileInput.addEventListener('change', readFile);
-  Papa.parse(fileInput.files[0], {
+  try {
+    Papa.parse(fileInput.files[0], {
     header: true,
     download: true,
     dynamicTyping: true,
     complete: function(results) {
       console.log(results);
       data = results.data;
-    }
-});
+    }})
+} catch (error) {
+  window.alert(error,"test");
+};
