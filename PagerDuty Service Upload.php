39ccf4aec6e99a23a0452b3c386515a31875e2b0
Jabbawocker <50668928+Jabbawocker@users.noreply.github.com>
2021-12-01 21:49:37 -0500

    Add files via upload

--- PagerDuty Service Upload.php
+++ PagerDuty Service Upload.php
@@ -2,14 +2,25 @@
 
 $API_ACCESS_KEY = 'u+5n_XdrViGnYFELTyxQ';
 
+$PAYLOAD = arry(
+    'service' => array(
+        'type' => 'string',
+        'name' => 'string',
+        'description' => 'string',
+        'auto_resolve_timeout' => 'integer'
+    )
+)
+
 $JSON = json_encode($PAYLOAD);
 
-$URL = 'https://api.pagerduty.com/services/';
+$URL = 'https://api.pagerduty.com/services';
 $session = curl_init();
 
 curl_setopt($session, CURLOPT_URL, $URL);
+curl_setopt($session, CURLOPT_HTTPHEADER, array(
     'Content-type: application/json',
-    'Authorization: Token token=' . $API_ACCESS_KEY;
+    'Authorization: Token token=' . $API_ACCESS_KEY
+));
 curl_setopt($session, CURLOPT_POSTFIELDS, $JSON);
 $output = curl_exec($session);
 ?>
